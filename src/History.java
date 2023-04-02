c9a8d2a5170e5a3846333d1f486757089c8e747c
Vitor Lobo Ramos <lobocode@gmail.com>
2023-04-01 22:54:34 -0300

    separe history/menu steps

--- src/History.java
+++ src/History.java
@@ -1,14 +1,14 @@
 public class History {
 
-    Player player = new Player();
-    String questionOne = (player.getName() + " estava andando de bike pela paulista e,"   +
-            "de repente, notei que ao invés de pedestres, haviam monstros, muitos monstros por" +
-            "toda parte. Esquivei-me de todos que consegui mas, infelizmente, logo a frente, "  +
-            "apareceu um que aparentou ser uma espécie de ZUMBI para me atacar. O que você "    +
-            "deseja fazer?");
-
+    public void HistoryOne() {
+        Player player = new Player();
+        String questionOne = (player.getName() + " estava andando de bike pela paulista e, de repente, notei que ao invés de pedestres, haviam monstros, muitos monstros por toda parte. Esquivei-me de todos que consegui mas, infelizmente, logo a frente, apareceu um que aparentou ser uma espécie de ZUMBI para me atacar. O que você deseja fazer?");
         System.out.println(questionOne);
+    }
+
+    public void MenuOne(){
         System.out.println("1 - Atacar");
         System.out.println("2 - Fugir");
         System.out.printf("Escolha entre a opção 1 e 2: ");
+    }
 }
